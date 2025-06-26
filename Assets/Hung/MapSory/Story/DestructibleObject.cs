641bdbaafe03a3a6b40b114a617bf308b1f7bcfa
phamviethungbmt <phamviethungbmt@gmail.com>
2025-06-26 23:38:31 +0700

    + Door Expolsive

---
+++ Assets/Hung/MapSory/Story/DestructibleObject.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class DestructibleObject : MonoBehaviour
+{
+    public int hpDoor = 10; 
+    private int currentHits = 0;
+
+    public GameObject explosionEffect;
+    public float explosiveTime = 2f;
+    public Transform explosivePostion;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("bullet"))
+        {
+            currentHits++;
+
+            Debug.Log("Bullet hit: " + currentHits + " / " + hpDoor);
+
+            // Tuỳ chọn: Huỷ viên đạn sau va chạm
+            //Destroy(collision.gameObject);
+
+            if (currentHits >= hpDoor)
+            {
+                DestroyObject();
+            }
+        }
+    }
+
+    private void DestroyObject()
+    {
+        if (explosionEffect != null && explosivePostion != null)
+        {
+            GameObject explosive = Instantiate(explosionEffect, explosivePostion.position, Quaternion.identity);
+            Destroy(explosive,explosiveTime);
+        }
+
+        // âm thanh nổ
+
+        Debug.Log(gameObject.name + " đã bị phá huỷ!");
+        Destroy(gameObject);
+    }
+}
