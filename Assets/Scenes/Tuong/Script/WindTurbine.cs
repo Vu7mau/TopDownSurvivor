c84dc45fdf5b0c320083867f94751dbaff8f528a
tuongnee007 <nguyenquoctuong007@gmail.com>
2025-08-24 18:09:26 +0700

    -fix vị trí đèn đường, add hiệu ứng xoay cối xoay gió

---
+++ Assets/Scenes/Tuong/Script/WindTurbine.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class WindTurbine : MonoBehaviour
+{
+    [Header("Cấu hình xoay")]
+    public float duration = 1f; 
+
+    private Tween rotateTween;
+
+    void Start()
+    {
+        rotateTween = transform
+            .DOLocalRotate(new Vector3(0f, 0f, 360f), duration, RotateMode.FastBeyond360)
+            .SetEase(Ease.Linear)
+            .SetLoops(-1, LoopType.Restart);
+    }
+
+    void OnDestroy()
+    {
+        if (rotateTween != null && rotateTween.IsActive())
+        {
+            rotateTween.Kill();
+        }
+    }
+}
