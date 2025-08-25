7a71987ed00a6ac6db2c9d9d75425cbf10ab5bfb
tuongnee007 <nguyenquoctuong007@gmail.com>
2025-08-25 09:03:30 +0700

    -Fix canvas pause không nhận sự kiện

---
+++ Assets/Scenes/Tuong/Script/ButtonHoverCheck.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonHoverCheck : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private bool isHovering = false;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isHovering = true;
+        Debug.Log("Chuột đang di vào nút!");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isHovering = false;
+        Debug.Log("Chuột đã rời khỏi nút!");
+    }
+
+    public bool IsHovering()
+    {
+        return isHovering;
+    }
+}
