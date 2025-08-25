cdc915e63be4e98ceca5e79b2776b273dee2f29b
tuongnee007 <nguyenquoctuong007@gmail.com>
2025-08-25 10:39:16 +0700

    +Add hiệu ứng đèn xe, add đèn xe

---
+++ Assets/Scenes/Tuong/Script/EmergencyLight.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+public class EmergencyLight : MonoBehaviour
+{
+    public Light redLight;
+    public Light blueLight;
+    public float flashSpeed = 5f; 
+    public float intensity = 15f;
+    void Update()
+    {
+        float t = Mathf.PingPong(Time.time * flashSpeed, 1f);
+
+        redLight.enabled = t < 0.5f;
+        blueLight.enabled = t >= 0.5f;
+
+        redLight.intensity = intensity;
+        blueLight.intensity = intensity;
+    }
+}
