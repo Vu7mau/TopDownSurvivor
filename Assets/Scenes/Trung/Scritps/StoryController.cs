36669816415f1c2e518a558a901d175145e66b64
tdlm987 <trungdlmpk03822@gmail.com>
2025-08-26 14:18:38 +0700

    + giết boss ở story unlock survival

---
+++ Assets/Scenes/Trung/Scritps/StoryController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryController : VuMonoBehaviour
+{
+    [SerializeField] protected EnemyAIController enemyAI;
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        this.CheckLastEnemiesDeath();
+    }
+
+    protected virtual void CheckLastEnemiesDeath()
+    {
+        StartCoroutine(this.CheckLastEnemiesDeathRoutine());
+    }
+
+    IEnumerator CheckLastEnemiesDeathRoutine()
+    {
+        if (enemyAI != null)
+        {
+            EnemyHealth enemyHealth = enemyAI.GetComponent<EnemyHealth>();
+            if (enemyHealth != null)
+            {
+                yield return new WaitUntil(() => enemyHealth.Health <= 0);
+
+                //do something
+                this.UnlockSurvivalMode();
+
+            }
+        }
+    }
+
+    protected virtual void UnlockSurvivalMode()
+    {
+        Debug.Log("Đã mở chế độ Sinh tồn!");
+        ModeUnlockManager.UnlockSurviveMode();
+        ModePanel.Instance?.RefreshUIInstant();
+    }
+}
