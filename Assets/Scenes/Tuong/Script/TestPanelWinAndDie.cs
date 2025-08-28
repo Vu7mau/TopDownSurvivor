c0c22d83014d60bc11e1e449798f5c411a3e5707
tuongnee007 <nguyenquoctuong007@gmail.com>
2025-08-28 10:03:24 +0700

    -fix tài nguyên dư

---
+++ Assets/Scenes/Tuong/Script/TestPanelWinAndDie.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPanelWinAndDie : MonoBehaviour
+{
+    public GameObject panelWin;
+    public GameObject panelDie;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (panelWin != null)
+                panelWin.SetActive(!panelWin.activeSelf);
+                Time.timeScale = 0f;
+
+            if (panelDie != null)
+                panelDie.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            if (panelDie != null)
+                panelDie.SetActive(!panelDie.activeSelf);
+                Time.timeScale = 0f;
+
+            if (panelWin != null)
+                panelWin.SetActive(false);
+
+        }
+    }
+}
